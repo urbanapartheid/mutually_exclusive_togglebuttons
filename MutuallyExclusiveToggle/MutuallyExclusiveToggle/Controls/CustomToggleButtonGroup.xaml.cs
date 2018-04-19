ec7447edf618dd054d21cbaf24b13d66478f88c6
Eric <urbanapartheid@gmail.com>
2018-04-19 11:52:45 -0400

    implemented custom toggle group

---
+++ MutuallyExclusiveToggle/MutuallyExclusiveToggle/Controls/CustomToggleButtonGroup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MutuallyExclusiveToggle.Controls
+{
+    public partial class CustomToggleButtonGroup : UserControl
+    {
+        #region CTOR
+        public CustomToggleButtonGroup()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Methods
+        private void ScreenToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            if(OutputToggle.IsChecked ?? false)
+                OutputToggle.IsChecked = false;
+        }
+
+        private void OutputToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            if(ScreenToggle.IsChecked ?? false)
+                ScreenToggle.IsChecked = false;
+        }
+        #endregion
+    }
+}
---
+++ MutuallyExclusiveToggle/MutuallyExclusiveToggle/ToggleControlVM.cs
@@ -0,0 +1,36 @@
+﻿namespace MutuallyExclusiveToggle
+{
+    public class ToggleControlVM
+    {
+        private bool _identifyOutputsActive;
+        private bool _identifyScreensActive;
+
+        public bool IdentifyOutputsActive
+        {
+            get
+            {
+                return _identifyOutputsActive;
+            }
+
+            set
+            {
+                if (_identifyOutputsActive != value)
+                    _identifyOutputsActive = value;
+            }
+        }
+
+        public bool IdentifyScreensActive
+        {
+            get
+            {
+                return _identifyScreensActive;
+            }
+
+            set
+            {
+                if (_identifyScreensActive != value)
+                    _identifyScreensActive = value;
+            }
+        }
+    }
+}
